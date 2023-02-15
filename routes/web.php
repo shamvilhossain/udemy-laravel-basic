ddf8ffc81a83e0e5d73dc8c041cda5b77889ead8
unknown <shamvil89@gmail.com>
2023-02-15 21:15:44 +0600

    about page backend

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::controller(HomeSliderController::class)->group(function () {
 
 Route::controller(AboutController::class)->group(function () {
     Route::get('/about/page', 'AboutPage')->name('about.page');
-
+    Route::post('/update/about', 'UpdateAbout')->name('update.about');
 
 });
 
