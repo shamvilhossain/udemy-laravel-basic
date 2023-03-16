7dee8e6972f9c818e3beb3f31f21952d31efcced
unknown <shamvil89@gmail.com>
2023-03-16 21:02:10 +0600

    portfolio part 2

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -14,4 +14,13 @@ class PortfolioController extends Controller
         $portfolio = Portfolio::latest()->get();
         return view('admin.portfolio.portfolio_all',compact('portfolio'));
     } // End Method
+
+    public function AddPortfolio(){
+        return view('admin.portfolio.portfolio_add');
+    } // End Method
+
+
+    public function StorePortfolio(Request $request){
+
+    }// End Method
 }
