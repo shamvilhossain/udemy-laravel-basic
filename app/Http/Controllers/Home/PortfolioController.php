be64a0458f7919a4297d197affe3a6f8e93ce8e1
unknown <shamvil89@gmail.com>
2023-04-14 15:44:39 +0600

    portfolio page

--- app/Http/Controllers/Home/PortfolioController.php
+++ app/Http/Controllers/Home/PortfolioController.php
@@ -132,5 +132,11 @@ class PortfolioController extends Controller
         return view('frontend.portfolio_details',compact('portfolio'));
      }// End Method
 
+    public function HomePortfolio(){
+
+        $portfolio = Portfolio::latest()->get();
+        return view('frontend.portfolio',compact('portfolio'));
+    } // End Method
+
 
 }
